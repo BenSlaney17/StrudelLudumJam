cc5c2e5094918e9e292836220c12a7c12c60e738
BenSlaney17 <1700801@uad.ac.uk>
2020-10-03 09:35:28 +0100

    Created Level Resetter - code not yet complete

---
+++ LudumJam47/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelReset : MonoBehaviour
+{
+    [SerializeField] GameEvent levelResetEvent;
+
+    public Transform[] pickupableObjects;
+
+    private Transform[] _startingTransforms;
+
+    private void Awake()
+    {
+        // get all starting transforms of pickup-able objects?
+    }
+
+    public void Reset()
+    {
+        // reset pickupable objects
+        ResetPickupableTransforms();
+
+        // reset timer
+
+        // reset player
+
+        levelResetEvent.Raise();
+    }
+
+    void ResetPickupableTransforms()
+    {
+        for (int i = 0; i < pickupableObjects.Length; i++)
+        {
+            pickupableObjects[i] = _startingTransforms[i]; 
+        }
+    }
+}
